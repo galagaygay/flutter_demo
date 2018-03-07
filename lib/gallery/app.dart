360e3b18d86643e9f46b26a58375c3a20a3dfa35
donghongyu <donghongyu@jsj.com.cn>
2018-03-07 12:59:37 +0800

    重新设计了项目中的层级划分方式。

---
+++ lib/gallery/app.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+//定义主的脚手架页面
+class GalleryApp extends StatefulWidget {
+  GalleryAppState createState() => new GalleryAppState();
+}
+
+//StatefulWidget[有状态控件]的逻辑和内部状态
+class GalleryAppState extends State<GalleryApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      //第一个位置放置了自定义的AppBar
+      appBar: new AppBar(
+        leading: new IconButton(
+          icon: new Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          tooltip: "导航菜单",
+          onPressed: null,
+        ),
+        title: new Text("主页面"),
+        centerTitle: true,
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              tooltip: "搜索",
+              onPressed: null
+          )
+        ],
+      ),
+      //Column垂直布局
+      body: new Column(
+        children: <Widget>[
+          //定义内容展示区域
+          //Expanded占满剩余空间的内容区域
+          new Expanded(
+            //创建了中心填充文本控件
+              child: new Center(
+                child: new Text("你好"),
+              )
+          ),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+          tooltip: "悬浮按钮",
+          child: new IconButton(
+              icon: new Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              onPressed: null
+          ),
+          onPressed: null
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/gallery/app.dart';
 
 //Dart程序执行的主入口，就像java中的main函数一样
 void main() {
@@ -7,49 +8,7 @@ void main() {
       new MaterialApp(
         title: "Demo",
         //指定app启动进入的根节点视图
-        home: new MainScaffold(),
+        home: new GalleryApp(),
       )
   );
-}
-
-//定义全局的文本控件
-var mTextView = new Text("你好，世界因为不同而精彩");
-
-//定义行视图中的子元素
-//var mColumnChildren=new List<Widget>{mTextView};
-
-//定义内容展示区域
-//Expanded占满剩余空间的内容区域
-var mContentView = new Expanded(
-  //创建了中心填充文本控件
-    child: new Center(
-      child: mTextView,
-    )
-);
-
-//Column垂直布局
-var mColumn = new Column(
-  children: <Widget>[
-    //第一个位置放置了自定义的AppBar
-    new AppBar(
-      leading: new IconButton(
-          icon: new Icon(Icons.menu),
-          tooltip: "导航栏",
-          onPressed: null
-      ),
-      title: new Text("Flutter编写的应用"),
-    ),
-    mContentView
-  ],
-);
-
-
-//定义主的脚手架页面
-class MainScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      child: mColumn,
-    );
-  }
 }
